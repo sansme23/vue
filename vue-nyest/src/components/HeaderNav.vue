a32b6974aa1d241bf6e0409b914a8688873427c1
sansme23 <145968593+sansme23@users.noreply.github.com>
2025-03-14 10:00:27 +0100

    oprettet HeaderNav komponent

---
+++ vue-nyest/src/components/HeaderNav.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HeaderNav'
+  }
+  </script>
+  
+  <style scoped>
+  header {
+    background-color: #333;
+    padding: 10px;
+  }
+  
+  nav ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around;
+  }
+  
+  nav ul li a {
+    color: white;
+    text-decoration: none;
+    font-size: 18px;
+  }
+  
+  nav ul li a:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
