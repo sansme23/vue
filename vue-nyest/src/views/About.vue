9dc8edc343dcedbba30166ea77c75221d61359c3
sansme23 <145968593+sansme23@users.noreply.github.com>
2025-03-14 11:05:51 +0100

    about.vue oprettet

---
+++ vue-nyest/src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h2>Om os</h2>
+      <p>Dette er undersiden om os.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About'
+  }
+  </script>
+  
