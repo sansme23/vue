c1f72055d7d2945bd9a51c23459da5342796be75
sansme23 <145968593+sansme23@users.noreply.github.com>
2025-03-14 11:07:10 +0100

    oprettet contact router

---
+++ vue-nyest/src/views/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h2>Kontakt os</h2>
+      <p>Dette er undersiden kontakt.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Contact'
+  }
+  </script>
+  
