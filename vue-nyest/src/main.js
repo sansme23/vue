a1649eea7f89bbdae437a1ce7b69b974a577f999
sansme23 <145968593+sansme23@users.noreply.github.com>
2025-03-14 11:08:51 +0100

    opdateret main.js for at kunne bruge routert

--- vue-nyest/src/main.js
+++ vue-nyest/src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router' // Importer routeren
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router) // Brug routeren
+  .mount('#app')
