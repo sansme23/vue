7c40f42a8ba445c86e28ff1a2d92eb6344c883ba
sansme23 <145968593+sansme23@users.noreply.github.com>
2025-03-14 11:04:40 +0100

    oprettet home.vue til routernav

---
+++ vue-nyest/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h2>Velkommen til Hjemmesiden!</h2>
+      <p>Dette er hovedsiden.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home'
+  }
+  </script>
+  
